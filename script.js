1b85f5ac5e491bb723994f9c788f08b7b6358564
Omead Musa <omeadmusa@hotmail.com>
2024-03-03 08:23:54 +1100

    removing chrome popup

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const button = document.querySelector('button');
-    button.addEventListener('click', () => alert('Access the leaderboard by logging in!'));
-});
-
-document.addEventListener('DOMContentLoaded', () => {
     const modal = document.getElementById('loginModal');
     const loginBtn = document.querySelector('button');
     const closeBtn = document.querySelector('.close-button');
